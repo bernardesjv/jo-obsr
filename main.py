5faeb1508d8ba12f39d07ed94f4fc87f6107940d
Jv.bsr. <joaovictorbernardes2017@gmail.com>
2025-04-02 17:56:18 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,153 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+'''
+num1 = int(input("digite um número : "))
+
+match num1:
+    case 1:
+        print("alimento nao perecivel")
+    case 2:
+        print("alimento perecivel")
+    case 3:
+        print("alimento perecivel")
+    case 4:
+        print("alimento perecivel")
+    case 5:
+        print("vestuário")
+    case 6:
+        print("vestuario")
+    case 7:
+        print("higiene pessoal")
+    case _:
+        if num1 <16 :
+            print("LImpeza e utensílios domésticos")
+        else :
+            print("Código invalido")
+            
+'''
+
+'''   
+num1 = int(input("código de produto: "))
+num2 = int(input("quantidade: "))
+match num1:
+        case 100:
+            print(num2 * 1.70)
+        case 101:
+            print(num2 * 2.30)
+        case 102:
+            print(num2 * 2.60)
+        case 103:
+            print(num2 * 2.40)
+        case 104:
+            print(num2 * 2.50)
+        case 105:
+            print(num2 * 1.00)
+'''         
+        
+'''       
+plsalario = input("plano de salario: ")
+sal = int(input("salario"))
+
+match plsalario:
+        case "A":
+            print(sal * 1.10)
+        case "B": 
+            print(sal * 1.15)
+        case "C":
+            print(sal * 1.20)
+            
+    
+'''
+
+'''
+num = int(input("digite um numero: "))
+
+match num:
+    case 1:
+        print("um")
+    case 2:
+        print("dois")
+    case 3:
+        print("três")
+    case 4:
+        print("quatro")
+    case 5:
+        print("cinco")
+    case 6:
+        print("seis")
+    case 7:
+        print("sete")
+    case 8:
+        print("oito")
+    case 9:
+        print("nove")
+    case 10:
+        print("dez")
+
+'''
+
+'''
+num = int(input("digite um numero: "))
+match num:
+    case 1:
+        print("domingo")
+    case 2:
+        print("segunda")
+    case 3:
+        print("terça")
+    case 4:
+        print("quarta")
+    case 5:
+        print("quinta")
+    case 6:
+        print("sexta")
+    case 7:
+        print("sabado")
+'''
+
+
+num = int(input("digite um numero: "))
+match num:
+    case 1:
+        print("janeiro")
+    case 2:
+        print("fevereiro")
+    case 3:
+        print("março")
+    case 4:
+        print("abril")
+    case 5:
+        print("maio")
+    case 6:
+        print("junho")
+    case 7:
+        print("julho")
+    case 8:
+        print("agosto")   
+    case 9:
+        print("setembro")
+    case 10:
+        print("outubro")
+    case 11:
+        print("novembro")
+    case 12:
+        print("dezembro")
+        
+        
+        
+        
+        
+        
