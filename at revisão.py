f765c309cc200c568397d01b1b01b21f6c076928
Jv.bsr. <joaovictorbernardes2017@gmail.com>
2025-04-09 22:13:12 -0300

    Add files via upload

---
+++ at revisão.py
@@ -0,0 +1,58 @@
+'''''
+num1 = int(input("numero 1 = "))
+num2 = int(input("numero 2 = "))
+
+x = num1 + num2 
+print(" X = ", x)
+'''''
+
+
+'''''
+R = int(input("digite o raio"))
+n = 3.14159
+area = n * R * 2
+A = round(area, 4)
+print("A=", A)
+'''''
+
+
+'''''
+A = int(input("digite um numero = "))
+B = int(input("digite outro = "))
+SOMA = A + B 
+print("SOMA",SOMA)
+'''''
+
+
+'''''
+A = float(input("digite a nota : "))
+B = float(input("digite a outra nota : "))
+
+A * 3.5
+B * 7.5
+
+MEDIA = A + B / 2
+C = round(MEDIA, 5) 
+print("MEDIA", C)
+'''''
+
+
+'''''
+pro1 = float(input("insira o codigo do produto : "))
+qt1 = float(input("insira a quantidade do produto : "))
+pre1 = float(input("preço do produto : "))
+
+pro2 = float(input("insira o codigo do segundo produto : "))
+qt2 = float(input("insira a quantidade do produto : "))
+pre2 = float(input("o preço do produto : "))
+
+   
+re1 = qt1 * pre1 
+re2 = qt2 * pre2 
+
+re3 = re1 + re2
+re4 = round(re3, 2)
+print("valor a pagar : R$",re4 )
+'''''
+
+
